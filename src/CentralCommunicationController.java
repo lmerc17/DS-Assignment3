58129c12b2ea60c3ea1b33ef7e381e9afc7fe740
lmerc17 <a1804049@adelaide.edu.au>
2023-11-06 13:55:36 +1030

    Made some changes to try to initialise each socket in a function, but I couldn't figure it out. I'm going to commit, so I save this version of code, but then I'm going to go back to the previous revision.

--- src/CentralCommunicationController.java
+++ src/CentralCommunicationController.java
@@ -7,9 +7,34 @@ import java.net.Socket;
 
 public class CentralCommunicationController {
 
+    public static boolean memberNodeInitialisation(Socket[] memberNodes, PrintWriter[] memberNodesOut, BufferedReader[] memberNodesIn, ServerSocket serverSocket){
+
+        try {
+            for (int i=0; i<9; i++) {
+                memberNodes[i] = serverSocket.accept();
+                memberNodesOut[i] = new PrintWriter(memberNodes[i].getOutputStream(), true);
+                memberNodesIn[i] = new BufferedReader(new InputStreamReader(memberNodes[i].getInputStream()));
+            }
+        }
+        catch(IOException e){
+            System.err.println("Exception caught when trying to listen on port 4567 or listening for a connection");
+            return false;
+        }
+
+        return true;
+
+    }
+
     public static void main(String[] args){
 
         int port = 4567;
+        ServerSocket serverSocket = new ServerSocket(port);
+        Socket[] memberNodes;
+
+        if(!memberNodeInitialisation(memberNodes, memberNodesOut, memberNodesIn, serverSocket)){
+            return;
+        }
+
         while(true) {
             try (ServerSocket serverSocket = new ServerSocket(port);
                  Socket clientSocket = serverSocket.accept();

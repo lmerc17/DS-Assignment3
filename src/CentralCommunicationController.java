ce5ccab5de5ac7768dd45c9d2634234598477913
lmerc17 <a1804049@adelaide.edu.au>
2023-11-06 14:54:52 +1030

    Managed to initialise, use and close all 9 sockets at once, made use of a bash file to run each of the java servers in the background.

--- src/CentralCommunicationController.java
+++ src/CentralCommunicationController.java
@@ -6,44 +6,79 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class CentralCommunicationController {
+    /** Method to initialise sockets upon Central Communication Controller startup
+     * @param serverSocket the server socket that will listen for the member nodes.
+     * @param memberSockets an array of sockets consisting of the member nodes.
+     * @param memberOuts an array of PrintWriters allowing data to be sent to the member nodes.
+     * @param memberIns an array of BufferedReaders allowing data to be read from the member nodes.
+     * @return an error code: 1 if the function fails, 0 otherwise.
+     */
+    public static int initialiseSockets(ServerSocket serverSocket, Socket[] memberSockets, PrintWriter[] memberOuts, BufferedReader[] memberIns){
+        try{
+            for(int i=0; i<9; i++){
+                memberSockets[i] = serverSocket.accept();
+                memberOuts[i] = new PrintWriter(memberSockets[i].getOutputStream());
+                memberIns[i] = new BufferedReader(new InputStreamReader(memberSockets[i].getInputStream()));
+            }
+        } catch (IOException e) {
+            System.err.println("Exception caught when trying to listen on port 4567 or listening for a connection");
+            return 1;
+        }
+        return 0;
+    }
 
-    public static boolean memberNodeInitialisation(Socket[] memberNodes, PrintWriter[] memberNodesOut, BufferedReader[] memberNodesIn, ServerSocket serverSocket){
-
+    /** Method to close sockets once finished using them
+     * @param serverSocket the server socket that will listen for the member nodes.
+     * @param memberSockets an array of sockets consisting of the member nodes.
+     * @param memberOuts an array of PrintWriters allowing data to be sent to the member nodes.
+     * @param memberIns an array of BufferedReaders allowing data to be read from the member nodes.
+     * @return an error code: 1 if the function fails, 0 otherwise.
+     */
+    public static int closeSockets(ServerSocket serverSocket, Socket[] memberSockets, PrintWriter[] memberOuts, BufferedReader[] memberIns){
         try {
-            for (int i=0; i<9; i++) {
-                memberNodes[i] = serverSocket.accept();
-                memberNodesOut[i] = new PrintWriter(memberNodes[i].getOutputStream(), true);
-                memberNodesIn[i] = new BufferedReader(new InputStreamReader(memberNodes[i].getInputStream()));
+            serverSocket.close();
+            for (int i = 0; i < 9; i++) {
+                memberSockets[i].close();
+                memberOuts[i].close();
+                memberIns[i].close();
             }
         }
         catch(IOException e){
-            System.err.println("Exception caught when trying to listen on port 4567 or listening for a connection");
-            return false;
+            System.err.println("Could not close sockets");
+            return 1;
         }
-
-        return true;
-
+        return 0;
     }
 
     public static void main(String[] args){
 
         int port = 4567;
-        ServerSocket serverSocket = new ServerSocket(port);
-        Socket[] memberNodes;
-
-        if(!memberNodeInitialisation(memberNodes, memberNodesOut, memberNodesIn, serverSocket)){
-            return;
-        }
 
         while(true) {
-            try (ServerSocket serverSocket = new ServerSocket(port);
-                 Socket clientSocket = serverSocket.accept();
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            ) {
-                System.out.println(in.readLine());
+            try{
+                // initialisation of all required sockets, inputs and output streams
+                ServerSocket serverSocket = new ServerSocket(port);
+                Socket[] memberSockets = new Socket[9];
+                PrintWriter[] memberOuts = new PrintWriter[9];
+                BufferedReader[] memberIns = new BufferedReader[9];
+
+                // function to initialise all the sockets
+                if(initialiseSockets(serverSocket, memberSockets, memberOuts, memberIns) == 1){
+                    return;
+                }
+
+                for(int i=0; i<9; i++){
+                    System.out.println("i: " + i + " " + memberIns[i].readLine());
+                }
+
+                // function to close all sockets once finished using them
+                if(closeSockets(serverSocket, memberSockets, memberOuts, memberIns) == 1){
+                    return;
+                }
+
+
             } catch (IOException e) {
-                System.err.println("Exception caught when trying to listen on port 4567 or listening for a connection");
+                System.err.println("Exception caught when trying to create sockets");
                 return;
             }
         }
---
+++ src/runNineClients.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# running 9 nodes at one time
+java MemberNode 1 true
+java MemberNode 2 true
+java MemberNode 3 true
+java MemberNode 4 true
+java MemberNode 5 true
+java MemberNode 6 true
+java MemberNode 7 true
+java MemberNode 8 true
+java MemberNode 9 true

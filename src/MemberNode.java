805a2aa9c58652d6b3fe370ab251954ef937d2e1
lmerc17 <a1804049@adelaide.edu.au>
2023-11-07 21:57:51 +1030

    added some stuff but then realised some issues. Will have to edit a lot of this tomorrow.

--- src/MemberNode.java
+++ src/MemberNode.java
@@ -8,32 +8,30 @@ import java.net.UnknownHostException;
 public class MemberNode {
 
     public static void main(String[] args){
-        // defining hostname and port number of centralised communication controller
-        String hostName = "localhost";
-        int portNumber = 4567;
 
-        // defining variable to keep track of proposal numbers
-        int proposalNumber = 1;
+        String hostName = "localhost"; // defining hostname of centralised communication controller
+        int portNumber = 4567; // defining port number of centralised communication controller
+        int proposalNumber = 1; // defining variable to keep track of proposal numbers
+        int newProposalNumber = 0; // defining variable to keep track of incoming new proposal numbers
+        boolean isProposer = false; // defining boolean to determine if this node is a proposer or not
+        int memberID = 0; // defining the integer that will store the ID of this node
+        int value = 0; // defining the value to be proposed by the node provided this is a proposer node
 
-        // defining variables to determine functionality for specific members
-        boolean isProposer = false;
-        int memberID = 0;
-
-        if(args.length != 2){
+        if(args.length != 2){ // checking correct number of arguments have been given
             System.err.println("Usage: java MemberNode <memberID> <proposer status>");
             return;
         }
 
-        try {
-            memberID = Integer.parseInt(args[0]);
+        if(args[1].equals("true")){ // if statement to check whether this node is the proposer or not
+            isProposer = true;
+        }
+        else if(!(args[1].equals("false"))) {
+            System.err.println("Please enter a valid proposer status");
+            return;
+        }
 
-            if(args[1].equals("true")) {
-                isProposer = true;
-            }
-            else if(!(args[1].equals("false"))) {
-                System.err.println("Please enter a valid proposer status");
-                return;
-            }
+        try { // try and catch to get memberID
+            memberID = Integer.parseInt(args[0]);
         }
         catch(NumberFormatException e){
             System.err.println("Please enter valid memberID");
@@ -46,31 +44,45 @@ public class MemberNode {
              BufferedReader in = new BufferedReader((new InputStreamReader(serverSocket.getInputStream())))
         )
         {
-            // requests can be in different formats depending on the message being sent
-            String request;
+            // initialisation of variables used to receive and send messages
+            String request; // requests can be in different formats depending on the message being sent
             String response;
             int recipient;
             int previous_proposal_number = 0;
             int previous_proposal_value = 0;
 
-            if(isProposer){
+            if(isProposer){ // if this node is the proposer, send a proposal request to the central communication controller
                 // proposal request is in the format of <type of message>:<sender>:<recipient>:<current proposal number>
-                request = "proposal:N" + memberID + ":broadcast:" + proposalNumber;
-                proposalNumber++;
-                out.println(request);
+
+                request = "proposal:N" + memberID + ":broadcast:" + proposalNumber; // create the proposal request
+                proposalNumber++; // increment the proposalNumber
+                out.println(request); // send the proposal request
+                //out.flush(); (here just in case I need it) -----------------------------------------------------------------------------------------------------------------------------
+
+                value = memberID; // value to be proposed is the ID of the member applying for presidency
+
+                /*
+                I have a feeling that I'm going to change this node so that it is in a listening state by default
+                Then when a message is received, it will be handled as necessary. That way I can send it to the
+                proposer or acceptor as needed. I will probably make use of functions to help make this look clean too.
+                */
+
+
+
             }
             else{
-                System.out.println("waiting for message...");
+                proposalNumber--; // decrement proposal so it is 0 and less than proposers initial proposal number of 1
                 response = in.readLine(); // wait for response
-                System.out.println(response);
                 recipient = Integer.parseInt(response.split(":")[1]);
-                if(proposalNumber < Integer.parseInt(response.split(":")[3])){
-                    // if proposal number received is greater than most recently accepted proposal, send back a promise message
+                newProposalNumber = Integer.parseInt(response.split(":")[3]);
+                if(newProposalNumber > proposalNumber){
+                    // if the new proposal number is greater than the old proposal number, send back a promise message and update proposal number
                     // promise is of format <type of message>:<sender>:<recipient>:<previous proposal number>:<previous proposal value>
                     request = "promise:N" + memberID + ":" + recipient + ":" + previous_proposal_number + ":" + previous_proposal_value;
+                    proposalNumber = newProposalNumber;
                 }
                 else{
-                    request = "promiseNack";
+                    request = "promiseNack:";
                 }
                 out.println(request);
             }

7be999d6b95c21ef63f0c2c15d7e315a0ecaeeb7
lmerc17 <a1804049@adelaide.edu.au>
2023-11-08 20:18:40 +1030

    Have edited MemberNode to be able to handle receiving proposal and promise messages.

--- src/MemberNode.java
+++ src/MemberNode.java
@@ -46,14 +46,11 @@ public class MemberNode {
             // initialisation of String request which stores the request message to be sent
             String request; // requests can be in different formats depending on the message being sent
             int proposalNumber = 0; // defining variable to keep track of proposal numbers
-            int value = 0; // defining the value to be proposed by the node provided this is a proposer node
-
 
             if(isProposer){ // if this node is the proposer, send a proposal request to the central communication controller
                 // proposal request is in the format of <type of message>:<sender>:<recipient>:<current proposal number>
 
                 request = "proposal:N" + memberID + ":broadcast:" + proposalNumber; // create the proposal request
-                proposalNumber++; // increment the proposalNumber
                 out.println(request); // send the proposal request
                 //out.flush(); (here just in case I need it) -----------------------------------------------------------------------------------------------------------------------------
             }
@@ -66,6 +63,8 @@ public class MemberNode {
             int previous_proposal_value = 0;
             int newProposalNumber; // defining variable to keep track of incoming new proposal numbers
             int currentProposalNumber = 0;
+            int currentProposalMember = 0; // used to check who the current proposal's proposer is
+            int value; // defining the value to be proposed by the node provided this is a proposer node
 
 
             while((response = in.readLine()) != null){ // wait for messages and when one comes in
@@ -73,9 +72,11 @@ public class MemberNode {
 
                 switch(messageType){
 
+                    // when a prepare message has been received (only received by acceptors)
                     case "prepare":
 
-                        recipient = Integer.parseInt(response.split(":")[1]);
+                        // find sender of prepare message to send message back to and new proposal number from incoming message
+                        recipient = Integer.parseInt(response.split(":")[1].substring(1));
                         newProposalNumber = Integer.parseInt(response.split(":")[3]);
 
                         // if the new proposal number is greater than the old proposal number,
@@ -83,16 +84,30 @@ public class MemberNode {
                         if(newProposalNumber > currentProposalNumber){
                             // promise is of format <type of message>:<sender>:<recipient>:<previous proposal number>:<previous proposal value>
                             request = "promise:N" + memberID + ":" + recipient + ":" + previous_proposal_number + ":" + previous_proposal_value;
-                            currentProposalNumber = newProposalNumber;
+                            currentProposalNumber = newProposalNumber; // update proposal number
                         }
                         else{
-                            request = "promiseNack:";
+                            request = "promiseNack:N" + memberID + ":" + recipient; // form promiseNack if not promising
                         }
-                        out.println(request);
+                        out.println(request); // send the promise message
 
                         break;
 
+                    // when a promise message has been received (only received by proposers)
                     case "promise":
+
+                        // if the received promise message has a previous proposal value
+                        if(Integer.parseInt(response.split(":")[4]) != 0){
+                            value = Integer.parseInt(response.split(":")[4]); // set value to that value
+                        }
+                        else{
+                            value = memberID; // otherwise set it to the memberID
+                        }
+
+                        // create an accept message with format <type of message>:<sender>:<recipient>:<proposal number>:<proposal value>
+                        request = "accept:N" + memberID + ":broadcast:" + proposalNumber + ":" + value;
+
+                        out.println(request); // send the accept message
 
                         break;
 
